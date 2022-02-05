3c5729b21ed11f724657b10a881bbab202103344
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2022-02-05 13:14:23 +0100

    add bold button and json getter

--- spec/Editor.spec.ts
+++ spec/Editor.spec.ts
@@ -1,4 +1,5 @@
 import * as webdriver from "selenium-webdriver"
+import { text } from "stream/consumers"
 
 describe('Editor', () => {
   it('return the expected placeholder after loading the writerjs library', async () => {
@@ -9,13 +10,50 @@ describe('Editor', () => {
 
                     
     await driver.get("file:///" + html_file)
-      .then(_ => driver.findElement({id: "wrapper"}))
-      .then(async (wrapper: webdriver.WebElement) => {
-        let placeholder = await wrapper.getAttribute("placeholder")
+      .then(_ => driver.findElement({id: "writer-js"}))
+      .then(async (textarea: webdriver.WebElement) => {
+        let placeholder = await textarea.getAttribute("placeholder")
         expect(placeholder).toEqual("Hello world")
       })
     
     await driver.quit()
   })
 
+
+  it('return bold button', async () => {
+    let html_file = __dirname + "/ui/editor.html"
+    let driver = new webdriver.Builder()
+                    .forBrowser("chrome")
+                    .build()
+
+                    
+    await driver.get("file:///" + html_file)
+      .then(_ => driver.findElement({id: "btn-bold"}))
+      .then(async (bold: webdriver.WebElement) => {
+        let id = await bold.getText()
+        expect(id).toEqual("B")
+      })
+    
+    await driver.quit()
+  })
+
+  it('return expected json after typing content', async () => {
+    let html_file = __dirname + "/ui/editor.html"
+    let driver = new webdriver.Builder()
+                    .forBrowser("chrome")
+                    .build()
+
+                    
+    await driver.get("file:///" + html_file)
+      .then(_ => driver.findElement({id: "writer-js"}))
+      .then(async (textarea: webdriver.WebElement) => {
+        textarea.clear()
+        textarea.sendKeys("foo bar")
+        let code = await driver.findElement({id: "code"}).getText()
+
+        expect(code).toEqual('{"content":"foo bar"}')
+      })
+    
+    await driver.quit()
+  })
 }) 
--- src/Editor.ts
+++ src/Editor.ts
@@ -1,17 +1,57 @@
 export class Editor {
-  static configure(hash: {target: string, placeholder: string}): void {
-    new Editor(hash).call()
+  static configure(hash: {target: string, placeholder: string}): Editor {
+    let editor = new Editor(hash)
+    editor.call()
+    
+    return editor
   }
 
-  target: string
+  wrapper: any
+  textarea: any
   placeholder: string
+  output: Object
 
   constructor(hash: {target: string, placeholder: string}) {
-    this.target = hash.target;
+    this.wrapper = document.querySelector(hash.target);
     this.placeholder = hash.placeholder;
+    this.output = {}
   }
 
-  private call() {
-    document.querySelector<HTMLTextAreaElement>(this.target).placeholder = this.placeholder
+  getJSON(): Object {
+    return this.output
+  }
+
+  private call(): void {
+    this.generateTextarea(this.wrapper)
+    this.menu()
+
+    this.textarea.placeholder = this.placeholder
+  }
+
+  private generateTextarea(wrapper: any): void {
+    let textarea = document.createElement('textarea')
+    textarea.id = "writer-js"
+    textarea.addEventListener('keyup', _ => {
+      this.output["content"] = this.textarea.value 
+    })
+    wrapper.appendChild(textarea)
+
+    this.textarea = textarea
+  }
+
+  private menu(): void {
+    let menu = document.createElement("div")
+    menu.id = "menu-writer-js"
+
+    let bold = document.createElement("button")
+    bold.id = "btn-bold"
+    bold.innerHTML = "<strong>B</strong>"
+
+    bold.addEventListener('click', () => {
+
+    })
+
+    menu.appendChild(bold)
+    this.wrapper.prepend(menu)
   }
 }
